38f7be6ffc2c93d675bf48d6aff62c6dbbcad9d7
adenantok <adenantok@gmail.com>
2024-11-06 15:55:22 +0700

    update update

--- models/buku.go
+++ models/buku.go
@@ -1,13 +1,18 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 // Buku is a struct that represents a book
 type Buku struct {
-	gorm.Model
-	Id    *int   `json:"id" gorm:"primaryKey;not null" `
-	Judul string `json:"judul" gorm:"not null" `
-	Harga *int   `json:"harga" gorm:"not null" `
+	Id        *int      `json:"id" gorm:"primaryKey;not null" `
+	Judul     string    `json:"judul" gorm:"not null" `
+	Harga     *int      `json:"harga" gorm:"not null" `
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime;<-:create"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 // Migrate migrates the schema
